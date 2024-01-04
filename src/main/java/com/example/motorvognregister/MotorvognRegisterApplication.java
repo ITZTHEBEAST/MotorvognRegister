3f149b37cabad2487f870d131739f273764ddf85
“ITZTHEBEAST” <fredrik.inpitak@gmail.com>
2024-01-04 12:48:44 +0100

    Initial commit

---
+++ src/main/java/com/example/motorvognregister/MotorvognRegisterApplication.java
@@ -0,0 +1,13 @@
+package com.example.motorvognregister;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotorvognRegisterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MotorvognRegisterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/motorvognregister/MotorvognRegisterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.motorvognregister;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MotorvognRegisterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
